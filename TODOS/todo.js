e97465c3cf8da2f4551bdbd366f9293b2ca2730e
skwd1013 <skwd1013@naver.com>
2023-11-22 14:58:01 +0900

    TODOS

---
+++ TODOS/todo.js
@@ -0,0 +1,128 @@
+//새로운 todo 추가하기 버튼 누르면 todo Create
+
+const todoList = document.getElementById("list");//list 요소 가져오기
+const createbtn = document.getElementById("create-btn");//create-btb 요소 가져오기
+let todos = [];//todo들의 배열
+
+
+createbtn.addEventListener('click', createTodo);
+
+
+function createTodo() {
+
+    const item = {                  //toto 속성
+        id: new Date().getTime(),//todo id
+        text: "",               //todo
+        complete: false         //todo complete 체크박스 상태
+    };
+
+    todos.unshift(item);
+    //itemEl = todo-list 아래 생길 div todo 창
+    //textEl = todo 안의 todo 텍스트
+    const { itemEl, textEl } = createElement(item);
+    // itemel은 createElement에서 생성한 itemEl 
+    // texdtel은 createElement에서 생성한 textEl 
+    todoList.prepend(itemEl);   //itemel은 리스트 앞쪽에 생성됨
+    textEl.removeAttribute("disabled"); //textel의 disabled 속성 제거 
+    textEl.focus(); 
+    //todo 생성후 텍스트 바로 입력하게 focus 함수 적용
+
+    saveToLocalStorage();   //localstorage에 저장
+
+}
+
+function createElement(item) {
+    //todo div 생성
+    const itemEl = document.createElement("div");
+    itemEl.classList.add("item");
+    //todo div 안의 checkbox 생성
+    const checkboxEl = document.createElement("input");
+    checkboxEl.type = "checkbox";
+    checkboxEl.checked = item.complete;//complete 값이 1이면 체크된 상태
+    
+    if (item.complete) {
+        itemEl.classList.add("complete");
+    }
+    //체크되었다면 complete 클래스로 변경
+
+    const textEl = document.createElement("input");
+    textEl.type = "text";
+    textEl.value = item.text;
+    textEl.setAttribute("disabled", "");
+
+    textEl.addEventListener("input", () => {
+        item.text = textEl.value;
+    });
+    textEl.addEventListener("blur", () => {
+        textEl.setAttribute("disabled", "")
+        saveToLocalStorage();
+    });
+    const actions = document.createElement("div"); //actions div생성 (edit,delete)
+    actions.classList.add("actions");
+
+    const editBtn = document.createElement("button");
+    editBtn.classList.add("icons");
+    editBtn.innerText = "edit";
+    editBtn.innerHTML="<img src=image/edit.jpg>";
+    editBtn.addEventListener("click", () => {   //edit 버튼 클릭 시 text입력할 수 있게 
+        textEl.removeAttribute("disabled"); //textel의 disabled 속성 제거 
+        textEl.focus();
+    })
+
+    checkboxEl.addEventListener("change", () => {
+        item.complete = checkboxEl.checked;
+        if (item.complete) {
+            itemEl.classList.add("complete");
+        }
+        else {
+            itemEl.classList.remove("complete");
+        }
+        saveToLocalStorage();//체크한거 기록해야함
+    });
+
+    const deleteBtn = document.createElement("button");
+    deleteBtn.classList.add("icons", "delete-btn");
+    deleteBtn.innerText = "delete";
+    deleteBtn.innerHTML="<img src=image/delete.jpg>";
+
+    deleteBtn.addEventListener("click", () => {   //edit 버튼 클릭 시 text입력할 수 있게 
+        todos = todos.filter(t => t.id != item.id)
+        itemEl.remove();
+        saveToLocalStorage();//지워진걸 기록해야함 
+    })
+
+    itemEl.appendChild(checkboxEl);
+    itemEl.appendChild(textEl);
+    itemEl.appendChild(actions);
+
+    actions.appendChild(editBtn);
+    actions.appendChild(deleteBtn);
+
+
+    return { itemEl, textEl, editBtn, deleteBtn };
+}
+
+function displayTodos() {
+    loadFromlocalStorage();
+
+    for (let i = 0; i < todos.length; i++) {
+        const item = todos[i];
+
+        const { itemEl } = createElement(item);
+        todoList.append(itemEl);
+    }
+}
+
+displayTodos();
+
+function saveToLocalStorage() {
+    const data = JSON.stringify(todos);
+    localStorage.setItem("my_todos", data);
+}
+
+function loadFromlocalStorage() {
+    const data = localStorage.getItem("my_todos");
+    if (data) {
+        todos = JSON.parse(data);
+    }
+}
--- Todo App/script.js
+++ Todo App/script.js
@@ -1,139 +1,66 @@
-
-
-
-
-// const list = document.getElementById('list');
-// const createBtn = document.getElementById('create-btn');
-
-// let todos = [{ status: 'checkbox', text: 'todo', update: 'update', delete: 'delete' }]
-
-// createBtn.addEventListener('click', createNewTodo);
-
-// function createNewTodo{
-//     //새로운 아이템 객체 생성
-//     const item = {
-//         id='11',
-//         text='',
-//         complete: true
-//     }
-
-//     todos.unshift(item);
-
-//     const {itemEl,textEl,editBtn,removeBtn}=createTodoElement(item);
-//     list.prepend(itemEl);
-//     textEl.removeAttribute('disabled');
-// }
-
-// function createTodoElement() {
-//     const itemEl = document.createElement('div');
-//     itemEl.classList.add('item');
-
-//     const checkboxEl = document.createElement('input');
-//     checkboxEl.type = 'checkbox';
-
-//     if (item.complete) {
-//         itemEl.classList.add('complete');
-//     }
-//     const textEl= document.createElement('input');
-//     textEl.type='text';
-//     textEl.value=item.text;
-//     textEl.setAttribute('disabled','');
-
-//     const actionsEl=document.createElement('div');
-//     actionsEl.classList.add('actions');
-
-//     const editBtn= document.createElement('button');
-//     editBtn.classList.add('material-material-icons');
-//     editBtn.innerText='edit';
-
-//     const removeBtn=document.createElement('button');
-//     removeBtn.classList.add('material-material-icons','remove-btn');
-//     removeBtn.innerText='remove_circles';
-
-//     actionsEl.append(editBtnBtn);
-//     actionsEl.append(removeBtn);
-
-//     itemEl.append(checkboxEl);
-//     itemEl.append(textElEl);
-//     itemEl.append(actionsEl);
-
-//     return{itemEl,textEl,editBtn,removeBtn}
-
-
-// }
-// 자바스크립트 적용이 필요한 부분
-// 1. todo만들기 버튼 클릭시 todo 나오기
-// 2. todo 안의 내용 만들기
-
-
-
-
-
 const list = document.getElementById("list");
-const createBtn=document.getElementById("create-btn");
+const createBtn = document.getElementById("create-btn");
 
-let todos=[];
+let todos = [];
 
-createBtn.addEventListener('click',createNewTodo);
+createBtn.addEventListener('click', createNewTodo);
 
 
-//추가 버튼을 눌렀을때 생성되는 빈 창
+//추가 버튼을 눌렀을때 생성되는 창
 function createNewTodo() {
-    // const item= {
-    //     id: new Date().getTime(),
-    //     text:"",
-    //     complete: false
-    // }
     const item = {
-		id: new Date().getTime(),
-		text: "",
-		complete: false
-	}
+        id: new Date().getTime(),
+        text: "",
+        complete: false
+    }
 
     todos.unshift(item);
 
-    const{itemEl,textEl} = createTodoElement(item);
+    const { itemEl, textEl } = createTodoElement(item);//todo element 생성
 
     list.prepend(itemEl);
 
     textEl.removeAttribute("disabled");
 
     textEl.focus();
-    
+
     saveToLocalStorage();
 }
 
+
+
+
+
+
+
 //추가 버튼을 누르면 안에 들어갈 내용들
-function createTodoElement(item){
-    const itemEl=document.createElement('div');
+function createTodoElement(item) {
+    const itemEl = document.createElement('div');
     itemEl.classList.add('item');
 
-    const checkboxel=document.createElement('input');
-    checkboxel.type="checkbox";
+    const checkboxel = document.createElement('input');
+    checkboxel.type = "checkbox";
     checkboxel.checked = item.complete;
 
-    if(item.complete){
+    if (item.complete) {
         itemEl.classList.add("complete");
     }
 
-    const textEl=document.createElement('input');
-    textEl.type="text";
-    textEl.value=item.text;
-    textEl.setAttribute("disabled","")
+    const textEl = document.createElement('input');
+    textEl.type = "text";
+    textEl.value = item.text;
+    textEl.setAttribute("disabled", "")
 
-
-
-
-    const actionsEl=document.createElement('div');
+    const actionsEl = document.createElement('div');
     actionsEl.classList.add("actions");
 
-    const editBtnEl= document.createElement('button');
+    const editBtnEl = document.createElement('button');
     editBtnEl.classList.add("material-icons");
-    editBtnEl.innerText="edit";
+    editBtnEl.innerText = "edit";
 
-    const removeBtnEl= document.createElement('button');
-    removeBtnEl.classList.add("material-icons","remove-btn" );
-    removeBtnEl.innerText="remove"
+    const removeBtnEl = document.createElement('button');
+    removeBtnEl.classList.add("material-icons", "remove-btn");
+    removeBtnEl.innerText = "remove"
 
     actionsEl.append(editBtnEl);
     actionsEl.append(removeBtnEl);
@@ -142,61 +69,65 @@ function createTodoElement(item){
     itemEl.append(textEl);
     itemEl.append(actionsEl);
 
-    checkboxel.addEventListener("change",() =>{
+    checkboxel.addEventListener("change", () => {
         item.complete = checkboxel.checked;
 
-        if(item.complete){
+        if (item.complete) {
             itemEl.classList.add("complete");
         }
-        else{
+        else {
             itemEl.classList.remove("complete")
         }
         saveToLocalStorage();
     });
 
-    textEl.addEventListener("input",()=>{
-        item.text=textEl.value;
+    textEl.addEventListener("input", () => {
+        item.text = textEl.value;
     });
-    textEl.addEventListener("blur",()=>{
-        textEl.setAttribute("disabled","")
+    textEl.addEventListener("blur", () => {
+        textEl.setAttribute("disabled", "")
         saveToLocalStorage();
     });
 
-    editBtnEl.addEventListener("click",()=>{
+    editBtnEl.addEventListener("click", () => {
         textEl.removeAttribute("disabled");
         textEl.focus();
     });
 
-    removeBtnEl.addEventListener("click",()=>{
+    removeBtnEl.addEventListener("click", () => {
         todos = todos.filter(t => t.id != item.id)
         itemEl.remove();
         saveToLocalStorage();
     });
 
-    return {itemEl,textEl,editBtnEl,removeBtnEl}
+    return { itemEl, textEl, editBtnEl, removeBtnEl }
 }
+
+
+
 //todo 보여주기
-function displayTodos(){
+function displayTodos() {
     loadFromlocalStorage();
 
-    for(let i =0; i<todos.length;i++){
+    for (let i = 0; i < todos.length; i++) {
         const item = todos[i];
 
-        const {itemEl} = createTodoElement(item);
+        const { itemEl } = createTodoElement(item);
         list.append(itemEl);
     }
 }
 
 displayTodos();
 
-function saveToLocalStorage(){
+function saveToLocalStorage() {
     const data = JSON.stringify(todos);
-    localStorage.setItem("my_todos",data);
+    localStorage.setItem("my_todos", data);
 }
 
-function loadFromlocalStorage(){
-    const data=localStorage.getItem("my_todos");
-    if(data){
-        todos=JSON.parse(data);
+function loadFromlocalStorage() {
+    const data = localStorage.getItem("my_todos");
+    if (data) {
+        todos = JSON.parse(data);
     }
+    
 }
