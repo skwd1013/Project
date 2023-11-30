718883111454434d9807facd5e376cc0685f642c
skwd1013 <skwd1013@naver.com>
2023-11-30 22:11:16 +0900

    spreadsheet

--- spreadsheet/index.js
+++ spreadsheet/index.js
@@ -1,52 +1,175 @@
-const SpreadSheetContainer = document.getElementById("spreadsheet-container");
+const exportBtn = document.querySelector("#export-btn");
+const SpreadSheetContainer = document.getElementById("SpreadSheet-container");
 const ROWS = 10;
 const COLS = 10;
 const spreadsheet = [];
-
+const alphabets = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 class Cell {
-  constructor(isHeader, disabled, data, row, column, active = false) {
+  constructor(
+    isHeader,
+    disabled,
+    data,
+    row,
+    column,
+    rowName,
+    columnName,
+    active = false
+  ) {
     this.isHeader = isHeader;
     this.disabled = disabled;
     this.data = data;
     this.row = row;
+    this.rowName = rowName;
+    this.columnName = columnName;
     this.column = column;
     this.active = active;
   }
 }
 initSpreadSheet();
 
+exportBtn.onclick = function (e) {
+  let csv = "";
+  for (let i = 0; i < spreadsheet.length; i++) {
+    if(i===0) continue;
+    csv +=
+      spreadsheet[i]
+        .filter((item) => !item.isHeader)
+        .map((item) => item.data)
+        .join(",") + "\r\n";
+  }
+  const csvObj = new Blob([csv]);
+  const csvUrl = URL.createObjectURL(csvObj);
+  console.log('csvUrl',csvUrl);
+
+  const a= document.createElement("a");
+  a.href = csvUrl;
+  a.download = "spreadsheet.csv";
+  a.click();
+};
 
 function initSpreadSheet() {
   for (let i = 0; i < ROWS; i++) {
     let spreadsheetRow = [];
     for (let j = 0; j < COLS; j++) {
-      const cell = new Cell(false,false,i+"-"+j,i,j,false);
-    
+      let cellData = "";
+      let isHeader = false;
+      let disabled = false;
+
+      if (j === 0) {
+        cellData = i;
+        isHeader = true;
+        disabled = true;
+      }
+      if (i === 0) {
+        isHeader = true;
+        disabled = true;
+        cellData = alphabets[j - 1]; //첫칸은 띄워져야 하므로 j-1
+      }
+      if (!cellData) {
+        cellData = "";
+      }
+      if (cellData <= 0) {
+        cellData = "";
+      }
+      const rowName = i;
+      const columnName = alphabets[j - 1];
+
+      const cell = new Cell(
+        isHeader,
+        disabled,
+        cellData,
+        i,
+        j,
+        rowName,
+        columnName,
+        false
+      );
       spreadsheetRow.push(cell);
     }
     spreadsheet.push(spreadsheetRow);
   }
-  drawSheet();
-  console.log(spreadsheet);
-}
 
+  console.log(spreadsheet);
+  drawSheet();
+}
 
 function createCellEl(cell) {
-  const cellEl = document.createElement("input")
-  cellEl.className = "cell"
-  cellEl.id = "cell_" + cell.row + cell.column
-  cellEl.value = cell.data
-  cellEl.disabled = cell.disabled
-  return cellEl
+  const cellEl = document.createElement("input");
+  cellEl.className = "cell";
+  cellEl.id = "cell_" + cell.row + cell.column;
+  cellEl.value = cell.data;
+  cellEl.disabled = cell.disabled;
+
+  if (cell.isHeader) {
+    cellEl.classList.add("header");
+  }
+  cellEl.onclick = () => handleClick(cell);
+  cellEl.onchange = (e) => handleOnChange(e.target.value, cell);
+  return cellEl;
+}
+function handleOnChange(data, cell) {
+  cell.data = data;
+}
+function handleClick(cell) {
+  clearHeaderActiveStates();
+  const columnHeader = spreadsheet[0][cell.column];
+  const rowHeader = spreadsheet[cell.row][0];
+  const columnHeaderEl = getElFromRowCol(columnHeader.row, columnHeader.column);
+  const rowHeaderEl = getElFromRowCol(rowHeader.row, rowHeader.column);
+  columnHeaderEl.classList.add("active");
+  rowHeaderEl.classList.add("active");
+  console.log("clicked cell", columnHeaderEl, rowHeaderEl);
+
+  document.querySelector("#cell-status").innerHTML = cell.columnName + cell.rowName
+
 }
 
+function getElFromRowCol(row, col) {
+  return document.querySelector("#cell_" + row + col);
+}
 function drawSheet() {
   for (let i = 0; i < spreadsheet.length; i++) {
-        for (let j = 0; j < spreadsheet[i].length; j++) {
-            const cell = spreadsheet[i][j];
-            SpreadSheetContainer.append(createCellEl(cell));
-      
+    const rowContainerEl = document.createElement("div");
+    rowContainerEl.className = "cell-row";
+    for (let j = 0; j < spreadsheet[i].length; j++) {
+      const cell = spreadsheet[i][j];
+      rowContainerEl.append(createCellEl(cell));
+    }
+    SpreadSheetContainer.append(rowContainerEl);
   }
 }
-}
 
+function clearHeaderActiveStates() {
+  const headers = document.querySelectorAll(".header");
+
+  headers.forEach((header) => {
+    header.classList.remove("active");
+  });
+}
